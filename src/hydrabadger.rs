221cd24c0748ce2377e04593dee766281fdca9f4
c0gent <nsan1129@gmail.com>
2018-07-08 16:23:10 -0700

    Send correct message upon connection.

--- src/hydrabadger.rs
+++ src/hydrabadger.rs
@@ -32,13 +32,14 @@ use tokio::{
     executor::{Executor, DefaultExecutor},
     prelude::*,
 };
-use tokio_codec::{Decoder, /*length_delimited*/};
+use tokio_codec::Decoder;
 use tokio_io::codec::length_delimited::Framed;
 use bytes::{BytesMut, Bytes, BufMut, IntoBuf, Buf};
 use rand::{self, Rng};
 use uuid::{self, Uuid};
 use byteorder::{self, ByteOrder, LittleEndian};
 use serde::{Serializer, Deserializer, Serialize, Deserialize};
+use serde_bytes;
 use bincode::{self, serialize_into, deserialize_from, serialize, deserialize};
 use tokio_serde_bincode::{ReadBincode, WriteBincode};
 
@@ -75,94 +76,22 @@ impl From<std::io::Error> for Error {
 }
 
 
-
-#[derive(Debug, Serialize, Deserialize)]
+/// Messages sent over the network between nodes.
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum WireMessage {
     Hello,
     Goodbye,
     Message,
-}
-
-
-
-mod codec {
-    use std::io;
-    use bytes::{BufMut, BytesMut};
-    use tokio_codec::{Encoder, Decoder};
-    use super::WireMessage;
-
-    /// A simple `Codec` implementation that just ships bytes around.
-    ///
-    /// This type is used for "framing" a TCP/UDP stream of bytes but it's really
-    /// just a convenient method for us to work with streams/sinks for now.
-    /// This'll just take any data read and interpret it as a "frame" and
-    /// conversely just shove data into the output location without looking at
-    /// it.
-    pub struct Bytes;
-
-    impl Decoder for Bytes {
-        type Item = BytesMut;
-        type Error = io::Error;
-
-        fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<BytesMut>> {
-            if buf.len() > 0 {
-                let len = buf.len();
-                Ok(Some(buf.split_to(len)))
-            } else {
-                Ok(None)
-            }
-        }
-    }
-
-    impl Encoder for Bytes {
-        type Item = Vec<u8>;
-        type Error = io::Error;
-
-        fn encode(&mut self, data: Vec<u8>, buf: &mut BytesMut) -> io::Result<()> {
-            buf.put(&data[..]);
-            Ok(())
-        }
-    }
-
-
-
-    // pub struct WireMessageCodec;
-
-    // impl Decoder for WireMessageCodec {
-    //     type Item = WireMessage;
-    //     type Error = io::Error;
-
-    //     fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<WireMessage>> {
-    //         // if buf.len() > 0 {
-    //         //     let len = buf.len();
-    //         //     Ok(Some(buf.split_to(len)))
-    //         // } else {
-    //         //     Ok(None)
-    //         // }
-
-    //         unimplemented!()
-    //     }
-    // }
-
-    // impl Encoder for WireMessageCodec {
-    //     type Item = WireMessage;
-    //     type Error = io::Error;
-
-    //     fn encode(&mut self, data: WireMessage, buf: &mut BytesMut) -> io::Result<()> {
-    //         // buf.put(&data[..]);
-    //         // Ok(())
-
-    //         unimplemented!()
-    //     }
-    // }
+    #[serde(with = "serde_bytes")]
+    Bytes(Bytes),
 }
 
 
 /// Transmit half of the message channel.
-type Tx = mpsc::UnboundedSender<Bytes>;
+type Tx = mpsc::UnboundedSender<WireMessage>;
 
 /// Receive half of the message channel.
-type Rx = mpsc::UnboundedReceiver<Bytes>;
+type Rx = mpsc::UnboundedReceiver<WireMessage>;
 
 type PeerTxs = Arc<RwLock<HashMap<SocketAddr, Tx>>>;
 
@@ -176,119 +105,7 @@ struct TimestampedMessage {
 }
 
 
-
-#[derive(Debug)]
-struct SocketBuffer {
-    /// The TCP socket.
-    socket: TcpStream,
-
-    /// Buffer used when reading from the socket. Data is not returned from this
-    /// buffer until an entire message has been read.
-    rd: BytesMut,
-
-    /// Buffer used to stage data before writing it to the socket.
-    wr: BytesMut,
-}
-
-impl SocketBuffer {
-    /// Create a new `SocketBuffer` codec backed by the socket
-    fn new(socket: TcpStream) -> Self {
-        SocketBuffer {
-            socket,
-            rd: BytesMut::new(),
-            wr: BytesMut::new(),
-        }
-    }
-
-    /// Buffer a message.
-    ///
-    /// This writes the message to an internal buffer. Calls to `poll_flush` will
-    /// attempt to flush this buffer to the socket.
-    fn buffer(&mut self, message: &[u8]) {
-        // Ensure the buffer has capacity. Ideally this would not be unbounded,
-        // but to keep the example simple, we will not limit this.
-        self.wr.reserve(message.len());
-
-        // Push the message onto the end of the write buffer.
-        //
-        // The `put` function is from the `BufMut` trait.
-        self.wr.put(message);
-    }
-
-    /// Flush the write buffer to the socket
-    fn poll_flush(&mut self) -> Poll<(), io::Error> {
-        // As long as there is buffered data to write, try to write it.
-        while !self.wr.is_empty() {
-            // Try to write some bytes to the socket
-            let n = try_ready!(self.socket.poll_write(&self.wr));
-
-            // As long as the wr is not empty, a successful write should
-            // never write 0 bytes.
-            assert!(n > 0);
-
-            // This discards the first `n` bytes of the buffer.
-            let _ = self.wr.split_to(n);
-        }
-
-        Ok(Async::Ready(()))
-    }
-
-    /// Read data from the socket.
-    ///
-    /// This only returns `Ready` when the socket has closed.
-    fn fill_read_buf(&mut self) -> Poll<(), io::Error> {
-        loop {
-            // Ensure the read buffer has capacity.
-            //
-            // This might result in an internal allocation.
-            self.rd.reserve(1024);
-
-            // Read data into the buffer.
-            let n = try_ready!(self.socket.read_buf(&mut self.rd));
-
-            if n == 0 {
-                return Ok(Async::Ready(()));
-            }
-        }
-    }
-}
-
-impl Stream for SocketBuffer {
-    type Item = BytesMut;
-    type Error = io::Error;
-
-    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-        trace!("Polling: {}", self.socket.peer_addr().unwrap());
-
-        // First, read any new data that might have been received off the socket
-        let sock_closed = self.fill_read_buf()?.is_ready();
-
-        // Now, try finding messages
-        let pos = self.rd.windows(2).enumerate()
-            .find(|&(_, bytes)| bytes == b"\r\n")
-            .map(|(i, _)| i);
-
-        if let Some(pos) = pos {
-            // Remove the message from the read buffer and set it to `message`.
-            let mut message = self.rd.split_to(pos + 2);
-
-            // Drop the trailing \r\n
-            message.split_off(pos);
-
-            // Return the message
-            Ok(Async::Ready(Some(message)))
-        } else {
-            if sock_closed {
-                Ok(Async::Ready(None))
-            } else {
-                Ok(Async::NotReady)
-            }
-        }
-    }
-}
-
-
-
+/// A stream/sink of `WireMessage`s connected to a socket.
 #[derive(Debug)]
 struct WireMessages {
     framed: Framed<TcpStream>,
@@ -299,6 +116,16 @@ impl WireMessages {
         WireMessages {
             framed: Framed::new(socket),
         }
+    }
+
+    pub fn socket(&self) -> &TcpStream {
+        self.framed.get_ref()
+    }
+
+    pub fn send_msg(&mut self, msg: WireMessage) -> Result<(), Error> {
+        self.start_send(msg)?;
+        let _ = self.poll_complete()?;
+        Ok(())
     }
 }
 
@@ -353,11 +180,8 @@ struct Peer {
     // /// Name of the peer.
     // name: BytesMut,
 
-    // /// The TCP socket wrapped with the `SocketBuffer` codec, defined below.
-    // wire_messages: SocketBuffer,
-
     // The incoming stream of messages:
-    wire_messages: SocketBuffer,
+    wire_messages: WireMessages,
 
     /// Handle to the shared message state.
     txs: PeerTxs,
@@ -370,9 +194,9 @@ struct Peer {
 
 impl Peer {
     /// Create a new instance of `Peer`.
-    fn new(txs: PeerTxs, wire_messages: SocketBuffer) -> Peer {
+    fn new(txs: PeerTxs, wire_messages: WireMessages) -> Peer {
         // Get the client socket address
-        let addr = wire_messages.socket.peer_addr().unwrap();
+        let addr = wire_messages.socket().peer_addr().unwrap();
 
         // Create a channel for this peer
         let (tx, rx) = mpsc::unbounded();
@@ -388,36 +212,37 @@ impl Peer {
             addr,
         }
     }
-}
 
+    /// Sends a message to all connected peers.
+    fn send_to_all(&mut self, msg: &WireMessage) {
+        // Now, send the message to all other peers
+        for (addr, tx) in self.txs.read().unwrap().iter() {
+            // Don't send the message to ourselves
+            if *addr != self.addr {
+                // The send only fails if the rx half has been dropped,
+                // however this is impossible as the `tx` half will be
+                // removed from the map before the `rx` is dropped.
+                tx.unbounded_send(msg.clone()).unwrap();
+            }
+        }
+    }
+}
 
 /// This is where a connected client is managed.
 ///
 /// A `Peer` is also a future representing completely processing the client.
 ///
-/// When a `Peer` is created, the first message (representing the client's name)
-/// has already been read. When the socket closes, the `Peer` future completes.
-///
 /// While processing, the peer future implementation will:
 ///
 /// 1) Receive messages on its message channel and write them to the socket.
-/// 2) Receive messages from the socket and broadcast them to all peers.
+/// 2) Receive messages from the socket and handle them appropriately.
 ///
 impl Future for Peer {
     type Item = ();
-    type Error = io::Error;
+    type Error = Error;
 
-    fn poll(&mut self) -> Poll<(), io::Error> {
-        // Tokio (and futures) use cooperative scheduling without any
-        // preemption. If a task never yields execution back to the executor,
-        // then other tasks may be starved.
-        //
-        // To deal with this, robust applications should not have any unbounded
-        // loops. In this example, we will read at most `MESSAGES_PER_TICK` messages
-        // from the client on each tick.
-        //
-        // If the limit is hit, the current task is notified, informing the
-        // executor to schedule the task again asap.
+    fn poll(&mut self) -> Poll<(), Error> {
+        // Ensure the loop can't hog the thread for too long:
         const MESSAGES_PER_TICK: usize = 10;
 
         // Receive all messages from peers.
@@ -428,7 +253,7 @@ impl Future for Peer {
                 Async::Ready(Some(v)) => {
                     // Buffer the message. Once all messages are buffered, they will
                     // be flushed to the socket (right below).
-                    self.wire_messages.buffer(&v);
+                    self.wire_messages.start_send(v)?;
 
                     // If this is the last iteration, the loop will break even
                     // though there could still be messages to read. Because we did
@@ -443,36 +268,16 @@ impl Future for Peer {
         }
 
         // Flush the write buffer to the socket
-        let _ = self.wire_messages.poll_flush()?;
+        let _ = self.wire_messages.poll_complete()?;
 
         // Read new messages from the socket
         while let Async::Ready(message) = self.wire_messages.poll()? {
             info!("Received message: {:?}", message);
 
             if let Some(msg) = message {
-                // Append the peer's name to the front of the message:
-                let mut msg_out = BytesMut::from(self.addr.to_string());
-                msg_out.extend_from_slice(b": ");
-                msg_out.extend_from_slice(&msg);
-                msg_out.extend_from_slice(b"\r\n");
-
-                // We're using `Bytes`, which allows zero-copy clones (by
-                // storing the data in an Arc internally).
-                //
-                // However, before cloning, we must freeze the data. This
-                // converts it from mutable -> immutable, allowing zero copy
-                // cloning.
-                let msg_out = msg_out.freeze();
-
-                // Now, send the message to all other peers
-                for (addr, tx) in self.txs.read().unwrap().iter() {
-                    // Don't send the message to ourselves
-                    if *addr != self.addr {
-                        // The send only fails if the rx half has been dropped,
-                        // however this is impossible as the `tx` half will be
-                        // removed from the map before the `rx` is dropped.
-                        tx.unbounded_send(msg_out.clone()).unwrap();
-                    }
+                match msg {
+                    WireMessage::Hello => info!("HELLO RECEIVED from '{}'", self.addr),
+                    _ => (),
                 }
             } else {
                 // EOF was reached. The remote client has disconnected. There is
@@ -502,7 +307,7 @@ pub fn handle_incoming(socket: TcpStream, peer_txs: PeerTxs) -> impl Future<Item
     let peer_addr = socket.peer_addr().unwrap();
     info!("Incoming connection from '{}'", peer_addr);
 
-    let wire_messages = SocketBuffer::new(socket);
+    let wire_messages = WireMessages::new(socket);
 
     wire_messages.into_future()
         .map_err(|(e, _)| e)
@@ -527,7 +332,6 @@ pub fn handle_incoming(socket: TcpStream, peer_txs: PeerTxs) -> impl Future<Item
 
             // Wrap `peer` with `Either::B` to make the return type fit.
             Either::B(peer)
-            // peer
         })
         .map_err(|e| {
             error!("Connection error = {:?}", e);
@@ -535,12 +339,12 @@ pub fn handle_incoming(socket: TcpStream, peer_txs: PeerTxs) -> impl Future<Item
 }
 
 
-// Used to create a secret key from a UUID.
-fn sum_into_u64(bytes: &[u8]) -> u64 {
-    let mut id_u64s = vec![0; 2];
-    LittleEndian::read_u64_into(bytes, &mut id_u64s);
-    id_u64s.iter().sum()
-}
+// // Used to create a secret key from a UUID.
+// fn sum_into_u64(bytes: &[u8]) -> u64 {
+//     let mut id_u64s = vec![0; 2];
+//     LittleEndian::read_u64_into(bytes, &mut id_u64s);
+//     id_u64s.iter().sum()
+// }
 
 
 pub struct Hydrabadger/*<T, N>*/ {
@@ -568,7 +372,7 @@ impl Hydrabadger {
         let id = Uuid::new_v4();
         let mut all_ids = BTreeSet::new();
         all_ids.insert(id);
-        let sk_share = sum_into_u64(id.as_bytes());
+        let sk_share = 0;
 
         let netinfo = NetworkInfo::new(
             id,
@@ -597,10 +401,7 @@ impl Hydrabadger {
         let socket = TcpListener::bind(&self.addr).unwrap();
         info!("Listening on: {}", self.addr);
 
-        // let connections_in = self.connections_in.clone();
         let peer_txs = self.peer_txs.clone();
-
-        // create LocalPeer
 
         let listen = socket.incoming()
             .map_err(|e| error!("failed to accept socket; error = {:?}", e))
@@ -608,7 +409,7 @@ impl Hydrabadger {
                 let peer_addr = socket.peer_addr().unwrap();
                 info!("Incoming connection from '{}'", peer_addr);
 
-                let wire_messages = SocketBuffer::new(socket);
+                let wire_messages = WireMessages::new(socket);
                 tokio::spawn(Peer::new(peer_txs.clone(), wire_messages)
                     .map_err(|e| {
                         error!("Connection error = {:?}", e);
@@ -623,33 +424,15 @@ impl Hydrabadger {
             for remote_addr in remotes.iter() {
                 let peer_txs = peer_txs.clone();
                 tokio::spawn(TcpStream::connect(remote_addr)
+                    .map_err(Error::from)
                     .and_then(move |socket| {
-                        let mut sck_buf = SocketBuffer::new(socket);
-                        // Create a message:
-                        let msg_out = BytesMut::from("Hihihihihi\r\n").freeze();
+                        // Wrap the socket with the frame delimiter and codec:
+                        let mut wire_messages = WireMessages::new(socket);
 
-                        BytesMut::from("Hihihihihi\r\n").freeze();
-
-                        // Buffer our message:
-                        sck_buf.buffer(&msg_out);
-                        // Flush the write buffer to the socket:
-                        sck_buf.poll_flush().unwrap();
-
-                        Peer::new(peer_txs, sck_buf)
-
-
-                        // // Serialize frames with bincode:
-                        // let serialized = WriteBincode::new(length_delimited);
-
-                        // // Send the hello value:
-                        // serialized.send(msg_out);
-
-                        // let msg_out = WireMessage::Hello;
-                        // // Delimit frames using a length header:
-                        // let wire_messages = Framed::new(socket);
-
-                        // Peer::new(peer_txs, wire_messages)
-
+                        match wire_messages.send_msg(WireMessage::Hello) {
+                            Ok(_) => Either::A(Peer::new(peer_txs, wire_messages)),
+                            Err(err) => Either::B(future::err(err)),
+                        }
                     })
                     .map_err(|err| error!("Socket connection error: {:?}", err)));
             }
